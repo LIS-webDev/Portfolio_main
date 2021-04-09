568d88da709f5cb2827a4b7e02acf37ea6335c20
Ivan Lazarev <lazarevivan777@gmail.com>
2021-04-10 01:09:39 +0500

    add script for progressBar

--- Portfolio_main/js/app.js
+++ Portfolio_main/js/app.js
@@ -11,6 +11,16 @@ function showMenu(className) {
 }
 
 showMenu('menu');
+
+function setProgress() {
+    const counters = document.querySelectorAll('.progress-block__count'),
+        progressLine = document.querySelectorAll('.progress-block__progress-bar span');
+    counters.forEach((item, i) => {
+        progressLine[i].style.width = item.innerHTML;
+    });
+}
+
+setProgress();
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
